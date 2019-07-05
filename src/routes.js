e5833c2c4b5e1f36e1823b1415edf2a612fe94d7
davimattos <dmattos340@gmail.com>
2019-07-05 10:10:46 -0300

    create auth middleware

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ import './database';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import AuthMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.put('/users', UserController.update);
+routes.put('/users', AuthMiddleware, UserController.update);
 routes.post('/sessions', SessionController.store);
 
 export default routes;
